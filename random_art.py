c18de9fc16ce8bb5f78a998ed77ac5ad43815c65
Andrew Fichman <andrew.fichman@gmail.com>
2022-01-15 21:09:15 -0500

    update

--- random_art.py
+++ random_art.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.transforms as transforms
 import random
 import numpy as np
 from PIL import Image
 
 MAX_X = 500
 MAX_Y = 500
+MAX_ANGLE = 360
 COLOR_WHITE = 255
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -15,13 +17,19 @@ def random_x_y():
     return random_coord(MAX_X), random_coord(MAX_Y)
 
 def random_width_height(x, y):
-    factor = 4
+    factor = 1
     def random_dim(coord, max_dim):
         return min(max_dim, int(random.random()*(max_dim-coord)))
     return (random_dim(x, MAX_X)/factor, random_dim(y, MAX_Y)/factor)
 
 def random_color():
     return tuple(random.random() for _ in range(3))
+
+def random_angle():
+    return int(random.random()*MAX_ANGLE)
+
+def center_rect(x, y, width, height):
+    return (x + width/2, y + height/2)
 
 def random_filename():
     out = []
@@ -46,6 +54,8 @@ def random_art():
         (width, height) = random_width_height(x, y)
         color = random_color()
         rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor=color, facecolor=color)
+        rot_trans = transforms.Affine2D().rotate_deg_around(*center_rect(x, y, width, height), random_angle())
+        rect.set_transform(rot_trans)
         print(f"(x, y, width, height, color) = ({x}, {y}, {width}, {height}, {color})")
 
         # Add the patch to the Axes
